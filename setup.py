653578cd153d2c2e9a2cc7a6ab3dca108e1a6950
hritikgupta7368 <hritikgupta8637@gmail.com>
2025-01-02 00:30:37 +0000

    added config in setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aws_devops_pipeline',
+    version='0.1.0',
+    description='A package for AWS DevOps Pipeline',
+    author='Hritik Gupta',
+    author_email='hritik@example.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
